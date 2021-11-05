9c5db6338b0b0afd0fac75ca1f70a3e43782754f
Jeff Terrell <jeffaterrell@gmail.com>
2021-11-05 17:48:22 -0400

    WIP: Working on toppingsPrice() logic to correctly count identical toppings

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,13 @@ Pizza.prototype.proteinPrice = function() {
 }
 
 Pizza.prototype.toppingsPrice = function() {
-  if (this.topping1 === "onions" || this.topping2 === "onions" || this.topping3 === "onions") {
+  if (this.topping1 === "onions") {
+    this.price +=1
+  }
+  if (this.topping2 === "onions") {
+    this.price +=1
+  } 
+  if (this.topping3 === "onions") {
     this.price +=1
   }
   if (this.topping1 === "peppers" || this.topping2 === "peppers" || this.topping3 === "peppers") {
@@ -113,4 +119,26 @@ $(document).ready(function() {
 
   // if (this.size === "undefined" || this.crust === "undefined") {
   //   return $("#error").show();
+  // }
+
+
+  // Pizza.prototype.toppingsPrice = function() {
+  //   if (this.topping1 === "onions" || this.topping2 === "onions" || this.topping3 === "onions") {
+  //     this.price +=1
+  //   }
+  //   if (this.topping1 === "peppers" || this.topping2 === "peppers" || this.topping3 === "peppers") {
+  //     this.price +=1
+  //   }
+  //   if (this.topping1 === "spinach" || this.topping2 === "spinach" || this.topping3 === "spinach") {
+  //     this.price +=1
+  //   }
+  //   if (this.topping1 === "mushrooms" || this.topping2 === "mushrooms" || this.topping3 === "mushrooms") {
+  //     this.price +=2
+  //   }
+  //   if (this.topping1 === "black olives" || this.topping2 === "black olives" || this.topping3 === "black olives") {
+  //     this.price +=2
+  //   }
+  //   if (this.topping1 === "pineapple" || this.topping2 === "pineapple" || this.topping3 === "pineapple") {
+  //     this.price +=2
+  //   }
   // }
